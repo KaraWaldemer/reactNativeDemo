1042366b8644d0f3910e5222ea04d937cbd6cd57
acbelz <andrew.christian.belzer@monsanto.com>
2017-12-12 18:54:46 -0600

    move toast call into native container

--- js/container/Native.container.android.js
+++ js/container/Native.container.android.js
@@ -1,11 +1,15 @@
 import { Text, View } from 'react-native'
 import React from 'react'
+import ToastExample from '../../android_modules/nativeModules'
+
 
 const NativeContainer = () => (
   <View>
     <Text>Native Code</Text>
   </View>
 )
+
+ToastExample.show('Awesome', ToastExample.SHORT)
 
 
 export default NativeContainer
